629ed1587aaa00ce450dda9f67fa9bb816c92feb
parpanironak <parpani.ronak@gmail.com>
2014-11-20 16:42:53 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bin/edu/nyu/cs/cs2580/CorpusAnalyzer$Factory.class
    	bin/edu/nyu/cs/cs2580/CorpusAnalyzer$HeuristicLinkExtractor.class
    	bin/edu/nyu/cs/cs2580/CorpusAnalyzer.class
    	bin/edu/nyu/cs/cs2580/CorpusAnalyzerPagerank.class
    	bin/edu/nyu/cs/cs2580/Document$HeuristicDocumentChecker.class
    	bin/edu/nyu/cs/cs2580/Document.class
    	bin/edu/nyu/cs/cs2580/DocumentFull.class
    	bin/edu/nyu/cs/cs2580/DocumentIndexed.class
    	bin/edu/nyu/cs/cs2580/Evaluator$DocumentRelevances.class
    	bin/edu/nyu/cs/cs2580/Evaluator.class
    	bin/edu/nyu/cs/cs2580/Grader.class
    	bin/edu/nyu/cs/cs2580/Indexer$Factory.class
    	bin/edu/nyu/cs/cs2580/Indexer.class
    	bin/edu/nyu/cs/cs2580/IndexerFullScan.class
    	bin/edu/nyu/cs/cs2580/LogMinerNumviews.class
    	bin/edu/nyu/cs/cs2580/QueryHandler$CgiArguments$OutputFormat.class
    	bin/edu/nyu/cs/cs2580/QueryHandler$CgiArguments$RankerType.class
    	bin/edu/nyu/cs/cs2580/QueryHandler$CgiArguments.class
    	bin/edu/nyu/cs/cs2580/QueryHandler.class
    	bin/edu/nyu/cs/cs2580/Ranker$Factory.class
    	bin/edu/nyu/cs/cs2580/Ranker.class
    	bin/edu/nyu/cs/cs2580/RankerFullScan.class
    	bin/edu/nyu/cs/cs2580/ScoredDocument.class
    	bin/edu/nyu/cs/cs2580/SearchEngine$Mode.class
    	bin/edu/nyu/cs/cs2580/SearchEngine$Options.class
    	bin/edu/nyu/cs/cs2580/SearchEngine.class
    	src/edu/nyu/cs/cs2580/Document.java
    	src/edu/nyu/cs/cs2580/DocumentIndexed.java
    	src/edu/nyu/cs/cs2580/ScoredDocument.java

--- src/edu/nyu/cs/cs2580/DocumentIndexed.java
+++ src/edu/nyu/cs/cs2580/DocumentIndexed.java
@@ -7,26 +7,18 @@ import java.util.HashMap;
  * information needed for your favorite ranker.
  */
 public class DocumentIndexed extends Document {
-<<<<<<< HEAD
-  private static final long serialVersionUID = 9184892508124423115L;
-
-  public DocumentIndexed(int docid) {
-    super(docid);
-  }
-=======
 	private static final long serialVersionUID = 9184892508124423115L;
 	private HashMap<String,Integer> snippet = new HashMap<String,Integer>();
 	
 	public HashMap<String, Integer> getSnippet() {
     return snippet;
-  }
+	}
 
-  public void setSnippet(HashMap<String, Integer> snippet) {
-    this.snippet = snippet;
-  }
+	public void setSnippet(HashMap<String, Integer> snippet) {
+		this.snippet = snippet;
+	}
 
-  public DocumentIndexed(int docid) {
+	public DocumentIndexed(int docid) {
 		super(docid);
 	}
->>>>>>> origin/master
 }
